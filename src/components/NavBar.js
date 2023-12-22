60ab6cc7f80938c75520c4b7d30b4cd56834672d
mykki92 <mike2108@hotmail.co.uk>
2023-12-22 17:06:22 +0000

    Add current user provider to the router

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,10 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/soundcheck-logo-light.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { CurrentUserContext } from "../App";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
 
 const NavBar = () => {
-  const currentUser = useContext(CurrentUserContext);
+  const currentUser = useCurrentUser();
 
   const loggedInIcons = <>{currentUser?.username}</>;
   const loggedOutIcons = (
