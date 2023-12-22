45b0499522e4d87016f7b4ac4d684774b94d9f07
mykki92 <mike2108@hotmail.co.uk>
2023-12-22 17:15:25 +0000

    Create axios interceptors to keep users logged in

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://soundcheck-drf-api-15efb796f01f.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
