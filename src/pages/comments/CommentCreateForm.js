5ac3c7c62fbf4220864c8f8eb199297c6774a0f1
mykki92 <mike2108@hotmail.co.uk>
2023-12-25 13:18:37 +0000

    Add comment component to post page

--- src/pages/comments/CommentCreateForm.js
+++ src/pages/comments/CommentCreateForm.js
@@ -50,7 +50,7 @@ function CommentCreateForm(props) {
           </Link>
           <Form.Control
             className={styles.Form}
-            placeholder="my comment..."
+            placeholder="..."
             as="textarea"
             value={content}
             onChange={handleChange}
