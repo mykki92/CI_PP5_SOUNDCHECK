6aa215ab799dccc26a4c85c80b6fe1decd064482
mykki92 <mike2108@hotmail.co.uk>
2023-12-25 13:12:46 +0000

    Add css styles for comment create and edit forms

--- src/pages/comments/CommentCreateForm.js
+++ src/pages/comments/CommentCreateForm.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 
+import styles from "../../styles/CommentCreateEditForm.module.css";
 import Avatar from "../../components/Avatar";
 import { axiosRes } from "../../api/axiosDefaults";
 
