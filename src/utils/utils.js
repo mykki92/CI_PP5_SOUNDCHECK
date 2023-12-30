a47484723c985c4250cc4264a051f23ad2623b45
mykki92 <mike2108@hotmail.co.uk>
2023-12-30 08:04:26 +0000

    Add comments for utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,10 @@
 import jwtDecode from "jwt-decode";
 import { axiosReq } from "../api/axiosDefaults";
 
+/*
+  Renders and updates different types of data for the InfiniteScroll component
+  Sends a request to the next page of results
+*/
 export const fetchMoreData = async (resource, setResource) => {
   try {
     const { data } = await axiosReq.get(resource.next);
@@ -16,51 +20,64 @@ export const fetchMoreData = async (resource, setResource) => {
   } catch (err) {}
 };
 
+/*
+  Increment the number of followers of profile clicked by 1
+  Increment the number of following users by 1
+*/
 export const followHelper = (profile, clickedProfile, following_id) => {
   return profile.id === clickedProfile.id
-    ? // This is the profile I clicked on,
-      // update its followers count and set its following id
+    ? 
       {
         ...profile,
         followers_count: profile.followers_count + 1,
         following_id,
       }
     : profile.is_owner
-    ? // This is the profile of the logged in user
-      // update its following count
+    ? 
       { ...profile, following_count: profile.following_count + 1 }
-    : // this is not the profile the user clicked on or the profile
-      // the user owns, so just return it unchanged
-      profile;
+    : profile;
 };
 
+/*
+  Decrement the number of followers of profile clicked by 1
+  Decrement the number of following users by 1
+*/
 export const unfollowHelper = (profile, clickedProfile) => {
   return profile.id === clickedProfile.id
-    ? // This is the profile I clicked on,
-      // update its followers count and set its following id
+    ? 
       {
         ...profile,
         followers_count: profile.followers_count - 1,
         following_id: null,
       }
     : profile.is_owner
-    ? // This is the profile of the logged in user
-      // update its following count
+    ?
       { ...profile, following_count: profile.following_count - 1 }
-    : // this is not the profile the user clicked on or the profile
-      // the user owns, so just return it unchanged
-      profile;
+    : profile;
 };
 
+/*
+  Sets a token timestamp in the browser storage
+  Accepts the data object returned by the API on login
+*/
 export const setTokenTimestamp = (data) => {
   const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
   localStorage.setItem("refreshTokenTimestamp", refreshTokenTimestamp);
 };
 
+/*
+  Indicates if users token should be refreshed or not
+  Returns the refreshTokenTimestamp value from our local storage
+  Token will be refreshed only for a logged in user
+*/
 export const shouldRefreshToken = () => {
   return !!localStorage.getItem("refreshTokenTimestamp");
 };
 
+/*
+  Remove the value from the local storage if the user logs out
+  or their refresh token has expired
+*/
 export const removeTokenTimestamp = () => {
   localStorage.removeItem("refreshTokenTimestamp");
 };
